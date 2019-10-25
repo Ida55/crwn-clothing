66c2cb5eb3696cc0ab9512393f738173d0e86425
ida.pousse <idapousse@yahoo.fr>
2019-10-25 12:44:41 +0200

    Apply background image & various modifications

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
+import './menu-item.styles.scss';
 
-const MenuItem = ({title}) => {
+
+const MenuItem = ({ title, image, size }) => {
     return ( 
-             <div className='menu-item'>
+            <div style={{ backgroundImage:`url(${image})` }} 
+            className={` ${size} menu-item`}>
                 <div className='content'>
-                    <h1 className='title'>{title}</h1>
+                    <h1 className='title'>{title.toUpperCase()}</h1>
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
