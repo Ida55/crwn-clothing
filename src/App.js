13f937f15b6073ec68960275b9ecb68eb81cd2c2
ida.pousse <idapousse@yahoo.fr>
2019-10-25 11:06:38 +0200

    Add components and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import './pages/homepage/homepage.styles.scss';
+import './components/menu-item/menu-item.styles.scss';
+import './components/directory/directory.styles.scss';
+import HomePage from './pages/homepage/homepage.component';
 
-import HomePage from './homepage.component';
 
 function App() {
   return <div>
-    <HomePage/>
+    <HomePage />
   </div>;
 }
 
